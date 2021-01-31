701622e00b3ac65d66e7f3b1d5b6a1024772a238
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2021-01-31 11:31:35 -0500

    Finished section 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+/* eslint no-eval: 0 */
+import {useState} from "react";
+import words from 'lodash.words';
 import Functions from "./components/Functions";
 import MathOperations from "./components/MathOperations";
 import Numbers from "./components/Numbers";
@@ -5,15 +8,27 @@ import Result from "./components/Result";
 
 import './App.css';
 
-const App = () => (
-    <main className="react-calculator">
-        <Result value="0"/>
-        <Numbers onClickNumber={(number: string) => console.log("OnClickNumber: ", number)}/>
-        <Functions onContentClear={() => console.log("OnContentClear")}
-                   onDelete={() => console.log("OnDelete")}/>
-        <MathOperations onClickOperation={(text: string) => console.log("Operation: ", text)}
-                        onClickEqual={(equal: string) => console.log("Equal: ",equal)}/>
-    </main>
-);
+const App = () => {
+    const [stack, setStack] = useState("");
+
+    const items = words(stack, /[^-^+^*^/]+/g);
+
+    return (
+        <main className="react-calculator">
+            <Result value={items.length > 0 ? items[items.length - 1] : '0'}/>
+
+            <Numbers onClickNumber={(number: string) => setStack(`${stack}${number}`)}/>
+
+            <Functions onContentClear={() => setStack("")}
+                       onDelete={() => {
+                           if (stack.length > 0)
+                               setStack(stack.substring(0, stack.length - 1))
+                       }}/>
+
+            <MathOperations onClickOperation={(text: string) => setStack(`${stack}${text}`)}
+                            onClickEqual={() => setStack(eval(stack).toString())}/>
+        </main>
+    );
+}
 
 export default App;
